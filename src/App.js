6ec089957d45fc8eb7b923b2a237ef9912f455e8
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-26 12:36:08 +0700

    setting App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import CreatePost from './pages/CreatePost'
+import MainPage from './pages/MainPage'
 
 function App() {
   return (
-    <div>HEY</div>
+    <>
+      <div className="navbar">
+        <div className="links">
+          <a href="/">Main Page</a>
+          <a href="/createpost">Create Page</a>
+        </div>
+      </div>
+
+      <Router>
+        <Route path="/" exact render={(props) => <MainPage />} />
+        <Route path="/createpost" render={(props) => <CreatePost />} />
+        {/* <Route path="/" /> */}
+      </Router>
+
+    </>
   );
 }
 
