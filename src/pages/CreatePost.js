48a0fd427a0e89ae0ef50b182399655daec834ee
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-04-01 14:15:46 +0700

    add submit post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 
 export default function CreatePost() {
+    const [userName, setUserName] = useState("")
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+
+    const submitPost = () => {
+
+    }
+
     return (
         <div className="CreatePost">
             <div className="uploadPost">
-                <input type="text" />
-                <input type="text" />
-                <textarea />
+                <label>Username</label>
+                <input type="text" onChange={(e) => { setUserName(e.target.value) }} />
+                <label>Title</label>
+                <input type="text" onChange={(e) => { setUserName(e.target.value) }} />
+                <label>Post Text</label>
+                <textarea onChange={(e) => { setUserName(e.target.value) }} />
+                <button onClick={submitPost}>Submit Post</button>
             </div>
         </div>
     )
